9b646e447a4f3fd82d76a56be71c6de9ef6075ad
SamBryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
2020-07-27 15:50:18 +0100

    function to read microexon results tables into dict

--- src/get_coverage_profiles.py
+++ src/get_coverage_profiles.py
@@ -18,11 +18,36 @@ first base returned by pileup function is the first base of the first read
 NOT guaranteed to be the first base or query region
 
 '''
-
+import csv
 import sys
 import pysam
 import os
 from collections import OrderedDict
+
+
+######-------------------------
+# Helper functions for main functions contained within this block
+######--------------------------
+
+def get_data_dict(row_list,key_idx_dict={'transcript_id': 1, 'splice_junctions': 3,'mex_sequence': 6}):
+    '''
+    returns dictionary of data of choice to return from row of microexon results table (row data, row_list, is stored in list by csv.reader).
+    Key names and index of column should be provided in key_idx_dict
+    '''
+    data_dict = {key: row_list[val] for key, val in key_idx_dict.items()}
+    return data_dict
+
+def mexonator_csv_to_dict(path):
+    '''
+    return nested dictionary of {microexon_coords: {'splice_junction': coords, 'mex_sequence': string, transcript = 'transcript'}} from microexonator results table
+    Table columns to extract are 1st (microexon_coords), 2nd (transcript id), 4th (splice_junction_coords) & 7th (mex_sequence)
+    '''
+
+    with open(path) as infile:
+        table_dict = {row[0]: get_data_dict(row) for row in csv.reader(infile, delimiter = "\t") if row[0] != 'ME'}
+        #!= ME my hacky way to skip header line
+    return table_dict
+
 
 def ref_name_add_brackets(string):
     '''
@@ -38,6 +63,51 @@ def ref_name_add_brackets(string):
     fixed_multi = split_str[1][:5] + '(' + split_str[1][5:] + ')'
 
     return '|'.join([split_str[0], fixed_multi, split_str[2]])
+
+#############----------------------
+# Main functions - loosely chronological order (check main for final order)
+#############----------------------
+
+
+def all_microexons_dict(filepaths_list, expected_files_list=['out.ambiguous.txt','out_filtered_ME.txt','out_low_scored_ME.txt','out_shorter_than_3_ME.txt','out.high_quality.txt']):
+    '''
+    Returns nested dictionary of all microexons as {microexon_coords: {'splice_junction': coords, 'mex_sequence': string, transcript = 'transcript'}}
+    Collated from all output files under Report/
+    All files listed in expected_files_list should have the same 'column indexes' for data want to extract
+    '''
+    full_dict = {}
+    for path in filepaths_list:
+        if os.path.basename(path) not in expected_files_list:
+            raise Exception("{0} could not be found in expected_files_list - {1} - which are known to have same column indexes. Was the correct file passed?".format(path,','.join(expected_files_list)))
+        else:
+            #dict of {microexon_coords: {'splice_junction': coords, 'mex_sequence': string, transcript = 'transcript'}} for given filepath
+            path_dict = mexonator_csv_to_dict(path)
+
+            for mex, mex_data in path_dict.items():
+                if mex not in full_dict.keys():
+                    full_dict[mex] = mex_data
+                else:
+                    #microexon has already been added to full dict - check if mex_data is identical to dict associated with mex in full_dict
+                    #if same skip to next microexon, otherwise stop execution
+                    if mex_data.get('splice_junction') == full_dict.get(mex).get('splice_junction'):
+                        if mex_data.get('mex_sequence') == full_dict.get(mex).get('mex_sequence'):
+                            continue
+                        else:
+                            raise Exception("microexon {0} has differently reported microexon sequences in different output files - please clarify".format(mex))
+
+                    elif mex_data.get('mex_sequence') == full_dict.get(mex).get('mex_sequence'):
+                        if mex_data.get('splice_junction') == full_dict.get(mex).get('splice_junction'):
+                            continue
+                        else:
+                            raise Exception("microexon {0} has differently reported splice junctions in different output files - please clarify".format(mex))
+                    else:
+                        raise Exception("microexon {0} has differently reported splice junctions and sequences throughout different output files. please check".format(mex))
+
+    return full_dict
+
+
+
+
 
 def initial_pileup(bam):
     '''
@@ -69,7 +139,6 @@ def initial_pileup(bam):
     #            pileup_dict[reference_tag] = reference_cov
 
     #    else:
-          
         reference_cov = {pileupcol.pos: pileupcol.n for pileupcol in bam.pileup(reference_tag)}
         pileup_dict[reference_tag] = reference_cov
 
@@ -130,13 +199,23 @@ def fill_pileup_dict(pileup_dict):
 
     return complete_pileup_dict
 
-
-
 if __name__ == '__main__':
 
     #bamfile = "/home/sam/cluster/sbs_projects/MicroExonator_fork/MicroExonator/Round2/Ward_CTL_1.bam"
     bamfile = sys.argv[1]
+    results_csvs = sys.argv[2]
+
+    #results csvs are all microexon results tables (under Report/), passed to script as comma-separated string
+    results_csvs = results_csvs.split(',')
+
+    #load results tables into dictionary of {microexon_coords: {'splice_junction': coords, 'mex_sequence': string, transcript = 'transcript'}}
+    microexon_info_dict = all_microexons_dict(results_csvs)
+    print(microexon_info_dict)
+    print(len(microexon_info_dict.keys()))
+    
+    '''
     bamfile = pysam.AlignmentFile(bamfile,"rb")
+
 
     print_list = ["chr16:28152785-28156073|MULTI2|100_AATATGGGGCTCCTGGTGAGGAACAGAAAG_100","chrY:9545006-9545124|ENST00000421178.2|100_90","chr17:42981074+42987255|ENST00000361677.5|100_GGCTTG_100"]
 
@@ -144,7 +223,7 @@ if __name__ == '__main__':
     bam_pileup_dict = initial_pileup(bamfile)
 
     for ref_name in print_list:
-        print(ref_name,bam_pileup_dict.get(ref_name))	
+        print(ref_name,bam_pileup_dict.get(ref_name))
 
     bamfile.close()
 
@@ -157,3 +236,4 @@ if __name__ == '__main__':
         print(ref_name,bam_pileup_dict.get(ref_name))
 
     #Now have nested dict of {ref_tag_name: OrderedDict(position: coverage)} where OrderedDict stores positions in ascending order!
+    '''
