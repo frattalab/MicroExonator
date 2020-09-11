3e5834ca9eba6b76178912a80ab284a72ceb9bcc
SamBryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
2020-09-11 14:32:05 +0100

    set csv field size to max

--- src/Get_ME_matches.py
+++ src/Get_ME_matches.py
@@ -3,10 +3,13 @@ import sys
 import csv
 
 
+#cryptics run - get error _csv.Error: field larger than field limit (131072) - this sets limit to maximum integer value variable can take
+csv.field_size_limit(sys.maxsize)
+
 def main(ME_centric):
-    
+
     header = ["ME", "U2_score", "Vertebrate_conservation", "ME_len", "ME_max_U2"]
-    
+
     print("\t".join(header))
 
     for row in csv.reader(open(ME_centric), delimiter = '\t'):
@@ -15,11 +18,11 @@ def main(ME_centric):
 
         ME_strand, ME_start, ME_end = ME.split("_")[-3:]
         ME_chrom =  "_".join(ME.split("_")[:-3])
-        
+
         #print(total_ME.split("|"))
-		
+
         for ME_match in total_ME.split(","):
-            
+
             print("\t".join(ME_match.split("|") + [len_micro_exon_seq_found, U2_scores]))
 
 if __name__ == '__main__':
